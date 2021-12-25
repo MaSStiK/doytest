2d4d359a138fa27e92742dfe9c3500b7ef476daa
Овчинников Матвей Яковлевич <pl8394@pl1.edu>
2021-12-25 13:53:59 +0500

    end

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -16,7 +16,8 @@ class TasksController extends Controller
         //
     }
 
-    public function add(Request $request) {
+    public function add(Request $request) { // Создать задачу
+
         $game_name = $request->input('name');
         $game_year = $request->input('year');
         $game_genree = $request->input('genre');
@@ -32,9 +33,42 @@ class TasksController extends Controller
         return response()->json($task, 201);
     }
 
-    public function get($task_id) {
+    public function get($task_id) { // Посмотреть задачу по id
 
         $task = Task::find($task_id);
+
+        return response()->json($task, 200);
+    }
+
+    public function edit(Request $request, $task_id) { // Отредактировать задачу по id
+        
+        $game_name = $request->input('name');
+        $game_year = $request->input('year');
+        $game_genree = $request->input('genre');
+        $game_developer = $request->input('developer');
+
+        $task = Task::find($task_id);
+        $task->name = $game_name;
+        $task->year = $game_year;
+        $task->genre = $game_genree;
+        $task->developer = $game_developer;
+        $task->save();
+
+        return response()->json($task, 200);
+    }
+
+    public function delete($task_id) { // Удалить задачу по id
+
+        $task = Task::find($task_id);
+        $task->delete();
+
+        return response()->json("Удалена задача с id " . $task_id, 200);
+    }
+
+    public function showAll() { // Посмотреть все задачи
+
+        $task = Task::all();
+
         return response()->json($task, 200);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,8 @@ $router->post('add', "TasksController@add");
 
 $router->get('get/{task_id}', "TasksController@get");
 
+$router->post('edit/{task_id}', "TasksController@edit");
+
+$router->get('delete/{task_id}', "TasksController@delete");
+
+$router->get('showAll', "TasksController@showAll");
